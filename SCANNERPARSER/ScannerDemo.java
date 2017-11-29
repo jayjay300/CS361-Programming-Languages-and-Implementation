a381f2cac45613aefcabcc4c548613309f6b99d9
jayjay300 <31739784+jayjay300@users.noreply.github.com>
2017-11-29 01:24:36 -0500

    Add files via upload

---
+++ SCANNERPARSER/ScannerDemo.java
@@ -0,0 +1,39 @@
+
+
+/*
+ * @author Christelle
+ * 
+ */
+public class ScannerDemo {
+
+	private static String file1 = "C:\\Users\\John\\Desktop\\file.txt";
+	private static int counter = 1;
+
+	public static void main(String args[]) {
+
+		TokenStream ts = new TokenStream(file1);
+
+		System.out.println(file1);
+		Token t;
+		//int tcount = 1;
+		while (!ts.isEndofFile()) {
+			t= ts.nextToken();
+			if(t == null || t.getValue() == ""){
+				if(!ts.isEndofFile()){								//also skipped comments are recognized as null. I believed comments should be seen as null, are they?
+					System.out.println("Token " + counter);
+			System.out.println("Token is null");	
+			counter++;
+				}
+			}
+			else{
+				//System.out.println("Token is not null"); necessary if every token is not null? comment out 
+				//System.out.println(t.toString());
+				System.out.println("Token " + counter);
+				System.out.print("Type: "+t.getType());
+				System.out.println("  Value: "+t.getValue());
+				counter++;
+			}
+			t.toString();
+		}
+	}
+}
